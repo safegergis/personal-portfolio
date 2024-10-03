b7c2cb55f65a6765ff128117c87b8a23e208bae1
Safe Gergis <safegerg@gmail.com>
2024-10-03 15:39:26 -0700

    added embla to featured image widget

--- components/image-widget.vue
+++ components/image-widget.vue
@@ -1,22 +1,46 @@
 <template>
-  <div class="my-4">
-    <Carousel :autoplay="2000" :breakpoints="carouselBreakpoints">
-      <Slide class="p-3" v-for="image in featuredImages" :key="image.id">
-        <NuxtImg
-          provider="cloudinary"
-          height="500"
-          :src="image.id"
-          fit="cover"
-        />
-      </Slide>
-      <template #addons>
-        <Navigation />
-      </template>
-    </Carousel>
+  <div class="flex my-4">
+    <button @click="onPrevious">
+      <Icon class="text-white w-10 h-10" name="lucide:arrow-left" />
+    </button>
+    <div class="overflow-hidden" ref="emblaRef">
+      <div class="flex">
+        <div
+          class="flex-[0_0_33%]"
+          v-for="image in featuredImages"
+          :key="image.id"
+        >
+          <NuxtImg
+            provider="cloudinary"
+            height="500"
+            :src="image.id"
+            fit="cover"
+          />
+        </div>
+      </div>
+    </div>
+    <button @click="onNext">
+      <Icon class="text-white w-10 h-10" name="lucide:arrow-right" />
+    </button>
   </div>
 </template>
 
 <script setup lang="ts">
+import useEmblaCarousel from "embla-carousel-vue";
+import autoPlay from "embla-carousel-autoplay";
+
+const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [
+  autoPlay({ delay: 5000 }),
+]);
+
+const onNext = () => {
+  if (emblaApi.value) emblaApi.value.scrollNext();
+};
+
+const onPrevious = () => {
+  if (emblaApi.value) emblaApi.value.scrollPrev();
+};
+
 interface ImageObject {
   id: string;
   tags: string[];
@@ -28,21 +52,6 @@ const props = defineProps<{
 const featuredImages = props.images.filter((image) =>
   image.tags.includes("featured")
 );
-
-const carouselBreakpoints = {
-  700: {
-    itemsToShow: 2,
-    wrapAround: true,
-  },
-  1200: {
-    itemsToShow: 4,
-    wrapAround: true,
-  },
-  1600: {
-    itemsToShow: 5,
-    wrapAround: true,
-  },
-};
 </script>
 
 <style></style>
